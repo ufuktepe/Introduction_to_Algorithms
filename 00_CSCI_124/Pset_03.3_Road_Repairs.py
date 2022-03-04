140444382d7c131112f2352c58ea7168163cd6e1
burak <b@gmail.com>
2022-03-04 08:13:28 -0800

    COmpleted Pset 3 Q3b.

--- 00_CSCI_124/Pset_03.3_Road_Repairs.py
+++ 00_CSCI_124/Pset_03.3_Road_Repairs.py
@@ -1,8 +1,6 @@
 import copy
-
 import networkx as nx
 import random
-
 
 class Vertex:
     def __init__(self, id):
@@ -71,12 +69,106 @@ class Graph:
 
         return max_st, total
 
+    def max_spanning_tree_edges(self, root):
+        # Num of vertices
+        n = len(self.vertices)
+
+        root.key = 0
+
+        # Create the set of vertices seen so far
+        s = set()
+
+        # Create the set of vertices seen so far
+        m = set()
+
+        for i in range(n):
+            max_dist = float('-inf')
+            u = None
+
+            for v in self.vertices:
+                if v.key > max_dist and v not in s:
+                    max_dist = v.key
+                    u = v
+
+            # Add the vertex to the max spanning tree
+            s.add(u)
+
+            if u.parent:
+                m.add((u.id, u.parent.id))
+
+            # Loop through u's adjacent vertices
+            for v, weight in u.adj.items():
+                if v not in s and v.key < weight:
+                    v.key = weight
+                    v.parent = u
+
+        return m
+
     def __str__(self):
         str_repr = 'Edges:\n'
         for vertex in self.vertices:
             if vertex.parent is not None:
                 str_repr += f'{vertex.id}-{vertex.parent.id} (weight:{vertex.key})\n'
         return str_repr
+
+
+def select(arr, k):
+    """
+    Select a pivot corresponding to the kth largest element in the array
+    :param arr: Input array
+    :param k: cardinality that represents the kth largest element in the array
+    :return: The value of the kth largest elm
+    """
+
+    # Divide array into chunks of 5
+    # chunks by taking i from 0 to 4, 5 to 9, 10 to 14, etc
+    chunks = [arr[i : i+5] for i in range(0, len(arr), 5)]
+
+    # sort each chunk
+    sorted_chunks = [sorted(chunk) for chunk in chunks]
+
+    # take the median of each chunk
+    medians = [chunk[len(chunk) // 2] for chunk in sorted_chunks]
+
+    # find the median of medians
+    if len(medians) <= 5:
+        pivot = sorted(medians)[len(medians) // 2 - 1]
+    else:
+        pivot = select(medians, len(medians) // 2 - 1)
+
+    # get the index of the pivot and create low and high arrays
+    p, arr_low, arr_high = partition(arr, pivot)
+
+    if k - 1 == len(arr_low):
+        #select that pivot
+        return pivot
+
+    elif k - 1 < len(arr_low):
+        #select a new pivot by looking on the left side of the partioning
+        return select(arr_low, k)
+    else:
+        #select a new pivot by looking on the right side of the partioning
+        return select(arr_high, k - (len(arr) - len(arr_high)))
+
+
+def partition(arr, pivot):
+    idx = 0
+    arr_low = []
+    arr_high = []
+    found_pivot = False
+    for i in range(len(arr)):
+        if pivot > arr[i]:
+            idx += 1
+            arr_low.append(arr[i])
+        elif pivot < arr[i]:
+            arr_high.append(arr[i])
+        elif found_pivot:
+            arr_low.append(arr[i])
+        else:
+            found_pivot = True
+
+    return idx, arr_low, arr_high
+
 
 
 if __name__ == '__main__':
@@ -102,39 +194,10 @@ if __name__ == '__main__':
 
     edges = copy.deepcopy(graph.edges)
 
-    # # Create My graph
-    # graph = Graph(set())
-    # vertices = []
-    #
-    # # Create Networkx graph
-    # G = nx.Graph()
-    #
-    # n = 400
-    #
-    # # Create vertices
-    # for i in range(n):
-    #     G.add_node(i)
-    #     v = Vertex(str(i))
-    #     graph.vertices.add(v)
-    #     vertices.append(v)
-    #
-    # # Create edges
-    # for i in range(n-1):
-    #     for j in range(i+1, n):
-    #         w = random.uniform(0, 1)
-    #         G.add_edge(i, j, weight=w)
-    #         graph.add_edge(vertices[i], vertices[j], w)
-    #
-    # # Networkx
-    # mst = nx.algorithms.maximum_spanning_edges(G, algorithm="prim", data=True)
-    # sum = 0
-    # for edge in mst:
-    #     sum += edge[2]['weight']
-    # print(f'Networknx total cost: {sum}')
-
-    mst, total = graph.remove_max_spanning_tree(a, edges)
-    # print(mst)
-    print(f'Total Cost: {total}')
+    # mst, total = graph.remove_max_spanning_tree(a, edges)
+    mst = graph.max_spanning_tree_edges(a)
+    print(mst)
+    # print(f'Total Cost: {total}')
 
     edge_weights = []
     for edge in edges:
@@ -148,7 +211,11 @@ if __name__ == '__main__':
 
     print(edge_weights)
 
-    treshold =
+    threshold = select(edge_weights, 5)
+
+    for edge_weight in edge_weights:
+        if edge_weight <= threshold:
+            print(edge_weight)
 
 
 
